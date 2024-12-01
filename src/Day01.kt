8571eb1b798778a69e9642f460d98abdeef18449
Maximilian Lindig <maximilian.lindig@gmail.com>
2024-12-01 13:15:05 +0100

    solved Star 1 of Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,78 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+  fun getLeftList(input: List<String>): MutableList<Int> {
+    val leftList = mutableListOf<Int>()
+    // get the numbers out of the file
+    for (line in input) {
+      var firstNumber: Int
+      val lineAsSequence = line.toCharArray()
+      val firstNumberAsSequence = CharArray(5)
+
+      for (i in 0..4) {
+        firstNumberAsSequence[i] = lineAsSequence[i]
+      }
+      firstNumber = String(firstNumberAsSequence).toInt()
+
+      leftList.add(firstNumber)
+    }
+    leftList.sort()
+
+    return leftList
+  }
+
+  fun getRightList(input: List<String>): MutableList<Int> {
+    val rightList = mutableListOf<Int>()
+
+    // get the numbers out of the file
+    for (line in input) {
+      var secondNumber: Int
+      val lineAsSequence = line.toCharArray()
+      val secondNumberAsSequence = CharArray(5)
+
+      for (i in 8..12) {
+        secondNumberAsSequence[i - 8] = lineAsSequence[i]
+      }
+      secondNumber = String(secondNumberAsSequence).toInt()
+
+      // add second value of line to rightList
+      rightList.add(secondNumber)
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    rightList.sort()
+
+    return rightList
+  }
+
+  /** What is the total distance between your lists? */
+  fun part1(input: List<String>): Int {
+
+    var totalDistance = 0
+
+    val leftList = getLeftList(input)
+    val rightList = getRightList(input)
+
+    for (i in 1..input.size) {
+      var difference = leftList[i - 1] - rightList[i - 1]
+
+      if (difference < 0) {
+        difference = difference * -1
+      }
+
+      totalDistance += difference
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+    return totalDistance
+  }
 
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+  /**
+   * Calculate a total similarity score by adding up each number in the left list after multiplying
+   * it by the number of times that number appears in the right list.
+   */
+  fun part2(input: List<String>): Int {
+    return input.size
+  }
 
-    // Read the input from the `src/Day01.txt` file.
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+  // Read the input from the `src/Day01.txt` file.
+  val input = readInput("Day01_input")
+  part1(input).println()
+  part2(input).println()
 }
